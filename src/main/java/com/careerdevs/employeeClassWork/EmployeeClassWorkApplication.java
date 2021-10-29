e62e02dc5c32dea366337f381b7d3dff118d9061
Tfauves <tfauvelle@gmail.com>
2021-10-29 15:25:55 -0400

    initial commit

---
+++ src/main/java/com/careerdevs/employeeClassWork/EmployeeClassWorkApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.employeeClassWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeClassWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeClassWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/employeeClassWork/EmployeeClassWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.employeeClassWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeClassWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
